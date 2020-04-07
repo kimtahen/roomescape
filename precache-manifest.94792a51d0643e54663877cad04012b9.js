dc3eefdd363ae2a0b626682c1f1e8b1e167e7ffe
kimtahen <kimtahen@naver.com>
2020-04-07 20:50:01 +0900

    Updates

---
+++ precache-manifest.94792a51d0643e54663877cad04012b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a0f570d4e8d94a39cb301d42675cb1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d5d5cbf69c5af66880d",
+    "url": "/static/js/2.779fd2fc.chunk.js"
+  },
+  {
+    "revision": "bc23c04eec3c55de9f1e7db1db146add",
+    "url": "/static/js/2.779fd2fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ab3123f7168d673f780",
+    "url": "/static/js/main.c6251466.chunk.js"
+  },
+  {
+    "revision": "cb9f7a8153704c695c89",
+    "url": "/static/js/runtime-main.18187429.js"
+  }
+]);
